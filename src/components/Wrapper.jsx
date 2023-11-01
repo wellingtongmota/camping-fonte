804d4b7d5110d555716b4e15a046ed268dc68c4a
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-11-01 14:33:13 -0300

    Alteração de design

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,17 +1,19 @@
-import { Flex, Heading } from '@chakra-ui/react'
 import React from 'react'
+import { Flex, Heading } from '@chakra-ui/react'
 
 const Wrapper = (props) => {
   return (
     <Flex
-      py={[8, 12]}
+      pt={[8, 12]}
       px={2}
       w='full'
       flexDirection='column'
       {...props}
     >
       {props.title !== undefined &&
-        <Heading textTransform='uppercase' color='teal.900' pb={6}>{props.title}</Heading>
+        <Flex justify={props.justify_title}>
+          <Heading textTransform='uppercase' color='teal.900' pb={6}>{props.title}</Heading>
+        </Flex>
       }
 
       {props.children}
