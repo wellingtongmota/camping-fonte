a8a3f139eb05017d07c7900093b4c1bb824d31b8
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-11-03 08:14:46 -0300

    Estilização do Badge

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,6 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 import '@fontsource/anton';
 // Supports weights 100-900
 import '@fontsource-variable/montserrat';
+import '@fontsource/poppins';
 
 const theme = extendTheme({
   components: {
@@ -10,6 +11,23 @@ const theme = extendTheme({
       baseStyle: {
         letterSpacing: '0.15rem'
       },
+    },
+    Button: {
+      baseStyle: {
+        fontWeight: 'bold', // Normally, it is "semibold"
+      },
+    },
+    Badge: {
+      baseStyle: {
+        color: 'teal.600',
+        bgColor: 'teal.50',
+        letterSpacing: '0.05rem',
+        borderRadius: 'full',
+        paddingInline: '1.25rem',
+        paddingBlock: '1rem',
+        width: 'fit-content',
+        fontFamily: `'Poppins', sans-serif`,
+      }
     },
   },
     styles: {
@@ -27,6 +45,8 @@ const theme = extendTheme({
     fonts: {
       heading: `'Anton', sans-serif`,
       body: `'Montserrat Variable', sans-serif`,
+      button: `'Montserrat Variable', sans-serif`,
+      // badge: `'Poppins', sans-serif`,
     },
   })
 
